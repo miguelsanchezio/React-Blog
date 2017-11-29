7315b9ade0ad19fc1e13c051085e1014a987353b
Miguel Sanchez <msanchez.anaheim@gmail.com>
2017-11-28 21:40:26 -0800

    Installed redux-form, created and added the PostsNew component, created and added the posts and form reducers

--- src/components/post_index.js
+++ src/components/post_index.js
@@ -1,13 +1,43 @@
 import React, { Component } from 'react'
+import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
+import { fetchPosts } from '../actions';
+import _ from 'lodash';
 
 class PostIndex extends Component {
-  render() {
-    return (
-      <div>
-        Post Index
-      </div>
-    );
-  }
+    componentDidMount() {
+        this.props.fetchPosts();
+    }
+
+    renderPosts() {
+        return _.map(this.props.posts, post => {
+            return (
+                <li className='list-group-item' key={post.id}>
+                    {post.title}
+                </li>
+            );
+        })
+    }
+
+    render() {
+        return (
+            <div>
+                <div className='text-xs-right'>
+                    <Link className='btn btn-primary' to='/posts/new'>
+                        Add a post
+                    </Link>
+                </div>
+                <h3>Posts</h3>
+                <ul className='list-group'>
+                    {this.renderPosts()}
+                </ul>
+            </div>
+        );
+    }
 }
 
-export default PostIndex;
+function mapStateToProps({posts}) {
+    return {posts};
+}
+
+export default connect(mapStateToProps, {fetchPosts})(PostIndex);
